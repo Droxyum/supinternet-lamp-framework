07223ee277448d5398757b8ccf51f9e79e4f8d42
Alexandre Simonet <contact@alexandresimonet.fr>
2016-01-20 08:35:14 +0100

    Add Constant

--- Web/index.php
+++ Web/index.php
@@ -8,4 +8,8 @@
 
 require_once '../vendor/autoload.php';
 
+define('ROOT_URL', str_replace('/index.php', '' , $_SERVER['SCRIPT_NAME']));
+define('BASE_URL', str_replace('/web', '' , ROOT_URL));
+define('ROOT_DIR', str_replace('/web/index.php', '' , $_SERVER['SCRIPT_FILENAME']));
+
 $Kernel = new \Core\Kernel();
