e6c27e78581005a79b3280cfd2dc06575cbce84a
Alexandre Simonet <contact@alexandresimonet.fr>
2016-01-20 11:23:48 +0100

    Add Twig

--- App/Controller/DefaultController.php
+++ App/Controller/DefaultController.php
@@ -9,11 +9,15 @@
 namespace App\Controller;
 
 
+use Core\Collection\StorageCollection;
 use Core\Controller\Controller;
+use Core\Http\Response\Response;
 
 class DefaultController extends Controller
 {
-    public function index() {
-
+    public function index(StorageCollection $params) {
+        return new Response('Default/index.html.twig', [
+            'name' => $params->get('name')
+        ]);
     }
 }
--- Core/Http/Response/Response.php
+++ Core/Http/Response/Response.php
@@ -11,5 +11,21 @@ namespace Core\Http\Response;
 
 class Response
 {
+    public function __construct($view, $params = [])
+    {
+        $loader = new \Twig_Loader_Filesystem(ROOT_DIR.'/App/Ressources/views/');
+        $viewFile = ROOT_DIR.'/App/Ressources/views/'.$view;
+        if (file_exists($viewFile)) {
+            $twig = new \Twig_Environment($loader, array(
+                'debug' => true,
+            ));
+            $twig->addExtension(new \Twig_Extension_Debug());
 
+            echo $twig->render($view, array_merge($params, [
+                'ROOT_URL' => ROOT_URL,
+                'BASE_URL' => BASE_URL,
+                'ROOT_DIR' => ROOT_DIR
+            ]));
+        }
+    }
 }
