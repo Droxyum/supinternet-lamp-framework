9f4f8fba7095e1e5ffee6c4cfe13109130ba74fa
Alexandre Simonet <contact@alexandresimonet.fr>
2016-01-20 17:00:05 +0100

    Fix DIC exception

--- Core/DIC/DIC.php
+++ Core/DIC/DIC.php
@@ -9,7 +9,60 @@
 namespace Core\DIC;
 
 
+use Core\Exception\ReflectionException;
+
 class DIC
 {
+    private $registry = [];
+    private $factories = [];
+    private $instances = [];
 
+
+    public function set($key, Callable $resolver) {
+        $this->registry[$key] = $resolver;
+    }
+
+    public function setFactory($key, Callable $resolver) {
+        $this->factories[$key] = $resolver;
+    }
+
+    public function setInstance($instance) {
+        $reflection = new \ReflectionClass($instance);
+        $this->instances[$reflection->getName()] = $instance;
+    }
+
+    public function get($key) {
+        if (empty($this->factories[$key])) {
+            return $this->factories[$key] = $this->registry[$key]();
+        }
+
+        if (empty($this->instances[$key])) {
+            if (!empty($this->registry[$key])) {
+                $this->instances[$key] = $this->registry[$key]();
+            } else {
+                $reflected_class = new \ReflectionClass($key);
+                if ($reflected_class->isInstantiable()) {
+                    $constructor = $reflected_class->getConstructor();
+                    if ($constructor) {
+                        $parameters = $constructor->getParameters();
+                        $constructor_parameters = [];
+                        foreach ($parameters as $parameter) {
+                            if ($parameter->getClass()) {
+                                $constructor_parameters[] = $this->get($parameter->getClass()->getName());
+                            } else {
+                                $constructor_parameters[] = $parameter->getDefaultValue();
+                            }
+                        }
+                        $this->instances[$key] = $reflected_class->newInstanceArgs($constructor_parameters);
+                    } else {
+                        $this->instances[$key] = $reflected_class->newInstance();
+                    }
+                } else {
+                    throw new ReflectionException('Class '.$reflected_class->getName().' cannot be instantiable');
+                }
+            }
+        }
+
+        return $this->instances[$key];
+    }
 }
