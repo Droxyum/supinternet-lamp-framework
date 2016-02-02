bd949d980f14d5ecc79bbded8f4564e018adcb45
Alexandre Simonet <contact@alexandresimonet.fr>
2016-02-02 19:00:35 +0100

    Add DIC in controller

--- Core/Controller/Controller.php
+++ Core/Controller/Controller.php
@@ -9,6 +9,7 @@
 namespace Core\Controller;
 
 
+use Core\DIC\DIC;
 use Core\Http\Request\Request;
 use Core\Orm\Orm;
 
@@ -16,9 +17,17 @@ class Controller
 {
     protected $Request;
     protected $Orm;
+    protected $container;
 
     public function __construct(Request $Request) {
         $this->Request = $Request;
         $this->Orm = new Orm();
+        $this->container = new DIC();
+        $this->fillContainer();
+    }
+
+    private function fillContainer() {
+        $this->container->setInstance($this->Request);
+        $this->container->setInstance($this->Orm);
     }
 }
